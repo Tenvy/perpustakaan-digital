876fe7e4947251e82ad47fff8edbf95e1cd875d0
tenvy <riskypabji@gmail.com>
2024-01-18 00:24:05 +0800

    feat: Logo Component for Sidebar Logo Section

---
+++ src/components/sidebar/logo.tsx
@@ -0,0 +1,10 @@
+
+const Logo = async () => {
+    return (
+        <div className='mx-2 text-primary-color font-bold text-4xl text-center pb-6'>
+            E-Library
+        </div>
+    )
+}
+
+export default Logo
