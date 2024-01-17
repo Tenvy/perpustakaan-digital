970eee0f7907597612c4be8df8f9fac91a48d856
tenvy <riskypabji@gmail.com>
2024-01-18 00:22:26 +0800

    chore: Menu Item Properties for Sidebar

---
+++ src/components/sidebar/menuProps.tsx
@@ -0,0 +1,14 @@
+import { MenuItemProps } from "@/type/sidebarMenu"
+
+import { BiBookContent as BookIcon } from 'react-icons/bi';
+
+const iconSize:number = 20;
+
+export const MENU_ITEMS: MenuItemProps[] = [
+  {
+    title: 'Discover',
+    href: '/',
+    icon: <BookIcon size={iconSize} />,
+    isShow: true,
+  },
+]; 
