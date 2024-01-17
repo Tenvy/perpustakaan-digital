3e49df5887cfdde98cb434801473711c1b838e36
tenvy <riskypabji@gmail.com>
2024-01-18 00:26:52 +0800

    feat: POST and GET http request for buku route

---
+++ src/app/api/buku/route.ts
@@ -0,0 +1,35 @@
+import { bukuType } from "@/type/buku";
+import prisma from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = await prisma.buku.findMany()
+        return NextResponse.json(response)
+    } catch (error) {
+        return NextResponse.json({msg: 'Something went wrong.', error})
+    }
+}
+
+export async function POST(request: Request){
+    const { Judul, Penulis, Penerbit, TahunTerbit }:bukuType = await request.json()
+    try {
+        const check = await prisma.buku.findUnique({
+            where: {
+                Judul
+            }
+        })
+        if (check) return NextResponse.json('Buku sudah ada!')
+        const response = await prisma.buku.create({
+            data: {
+                Judul,
+                Penulis,
+                Penerbit,
+                TahunTerbit
+            }
+        })
+        return NextResponse.json({msg: 'Buku Berhasil Dibuat!',response})
+    } catch (error) {
+        return NextResponse.json({msg: 'Something went wrong.', error})
+    }
+}
