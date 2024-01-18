61379b2bc552477d1b4a0f9342022e9fca2d8721
tenvy <riskypabji@gmail.com>
2024-01-18 13:34:20 +0800

    feat: Discover Component

---
+++ src/app/components/discover.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/elements/container"
+import CardSection from "./cardSection"
+
+const Discover = () => {
+    return (
+        <Container>
+            <CardSection/>
+        </Container>
+    )
+}
+
+export default Discover
