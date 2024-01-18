c24fea5325d7da9467725a7af765d8e23bc4fb57
tenvy <riskypabji@gmail.com>
2024-01-18 13:36:20 +0800

    feat: add discover to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "@/components/sidebar/sidebar";
+import Discover from "./components/discover";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex">
       <Sidebar/>
+      <Discover/>
     </main>
   )
 }
