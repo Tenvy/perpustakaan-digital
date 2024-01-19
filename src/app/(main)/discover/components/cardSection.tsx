a52678443576870fe32e4c47c9175db9a378d470
tenvy <riskypabji@gmail.com>
2024-01-19 23:39:13 +0800

    feat: moved card Section component from out main route groups

---
+++ src/app/(main)/discover/components/cardSection.tsx
@@ -0,0 +1,35 @@
+import Card from "@/components/elements/card"
+import Image from "next/image"
+import { getBook } from "@/services/book"
+import { bukuType } from "@/type/buku"
+import Link from "next/link"
+
+const CardSection = async () => {
+    const data = await getBook()
+
+    return (
+        <div className="grid grid-cols-6 gap-4">
+            {data?.map((res: bukuType, key: number) => {
+                return (
+                    <Link href={`/discover/${res.BukuID}`} key={key}>
+                        <Card className="flex">
+                            <div className=" mx-auto p-2 flex flex-col gap-2">
+                                <Image src={res.Gambar} alt="Gambar-Buku" width={200} height={600} className="border" />
+                                <div className="text-primary-color">
+                                    <div className="text-lg font-bold">
+                                        {res.Judul}
+                                    </div>
+                                    <div className="">
+                                        {res.Penulis}
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+
+export default CardSection
