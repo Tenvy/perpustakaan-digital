ab45c15426cb511dc5e3f716433e50669cd1ffdd
tenvy <riskypabji@gmail.com>
2024-01-19 23:38:49 +0800

    feat: moved discover component from out main route groups

---
+++ src/app/(main)/discover/components/discover.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/elements/container"
+import CardSection from "./cardSection"
+
+const Discover = () => {
+    return (
+        <>
+            <CardSection/>
+        </>
+    )
+}
+
+export default Discover
