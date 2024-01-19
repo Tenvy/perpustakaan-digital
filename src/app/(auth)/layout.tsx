8ff1d7d028e2eccd82579bfa32093a0ee84b67b9
tenvy <riskypabji@gmail.com>
2024-01-19 23:42:37 +0800

    feat: layout for auth route group

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import Provider from '@/provider/AuthProvider';
+import { getServerSession } from 'next-auth'
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Perpustakaan Web',
+  description: 'Perpustakaan Web',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = getServerSession()
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <Provider session={session}>
+        {children}
+      </Provider>
+      </body>
+    </html>
+  );
+}
