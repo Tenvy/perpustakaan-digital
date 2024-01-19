100340e415033922710f958986d9088bfcfbfadf
tenvy <riskypabji@gmail.com>
2024-01-19 23:40:57 +0800

    feat: moved layout from out main route groups

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import Provider from '@/provider/AuthProvider';
+import { getServerSession } from 'next-auth'
+import Sidebar from '@/components/sidebar/sidebar';
+import Container from '@/components/elements/container';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Perpustakaan Web',
+  description: 'Perpustakaan Web',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = getServerSession()
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider session={session}>
+          <div className='flex'>
+            <Sidebar />
+            <Container>
+              {children}
+            </Container>
+          </div>
+        </Provider>
+      </body>
+    </html>
+  );
+}
