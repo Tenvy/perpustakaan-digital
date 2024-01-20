b34d9dc5f0722ace07a3a474f54cddeb2d0b2a0a
tenvy <riskypabji@gmail.com>
2024-01-21 00:00:21 +0800

    feat: Book Pages

---
+++ src/app/(main)/buku/page.tsx
@@ -0,0 +1,11 @@
+import BookDataSection from "./components/bookDataSection"
+
+const Page = () => {
+    return (
+        <div className="bg-secondary-color rounded-xl p-4 text-primary-color">
+            <BookDataSection/>
+        </div>
+    )
+}
+
+export default Page
