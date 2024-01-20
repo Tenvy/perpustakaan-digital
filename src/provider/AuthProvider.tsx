f68823d44c0030fe8f22ac2a324d9e100d49841d
tenvy <riskypabji@gmail.com>
2024-01-20 23:55:16 +0800

    feat: edgeStore Provider

--- src/provider/AuthProvider.tsx
+++ src/provider/AuthProvider.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { SessionProvider } from "next-auth/react"
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react' 
+import { type EdgeStoreRouter } from '../app/api/edgestore/[...edgestore]/route';
+import { createEdgeStoreProvider } from '@edgestore/react';
 
 interface ProviderProps {
   children: ReactNode,
@@ -16,3 +18,8 @@ const Provider = ({ children, session }: ProviderProps) => {
 }
 
 export default Provider
+
+const { EdgeStoreProvider, useEdgeStore } =
+  createEdgeStoreProvider<EdgeStoreRouter>();
+
+export { EdgeStoreProvider, useEdgeStore };
