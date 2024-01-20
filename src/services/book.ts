bbda2b3cc217b906c1da9e0e28ddce6a60feff98
tenvy <riskypabji@gmail.com>
2024-01-20 23:54:28 +0800

    feat: post, delete, patch service for book

--- src/services/book.ts
+++ src/services/book.ts
@@ -1,11 +1,51 @@
+import { bukuType } from "@/type/buku";
+
 const getBook = async () => {
-    const response = await fetch( process.env.NEXTAUTH_URL + `/api/buku`, { method: 'GET', cache: "no-store"})
-    return response.json()
-}
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku`, {
+    method: "GET",
+    cache: "no-store",
+  });
+  return response.json();
+};
 
 const getBookById = async (id: string) => {
-    const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku/${id}`, {method: 'GET', cache: 'no-store'})
-    return response.json()
-}
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku/${id}`, {
+    method: "GET",
+    cache: "no-store",
+  });
+  return response.json();
+};
 
-export { getBook, getBookById }
+const postBook = async (data: bukuType) => {
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku`, {
+    method: "POST",
+    cache: "no-store",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+};
+
+const deleteBook = async (id: string) => {
+    const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku/${id}`, {
+      method: "DELETE",
+      cache: "no-store",
+    });
+    return response.json();
+  };
+
+  const patchBook = async (data: bukuType, id: string) => {
+    const response = await fetch(process.env.NEXTAUTH_URL + `/api/buku/${id}`, {
+      method: "PATCH",
+      cache: "no-store",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  };
+
+export { getBook, getBookById, postBook, deleteBook, patchBook };
