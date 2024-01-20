cf531a82ace7b78db6381707b6e810ce6c94a374
tenvy <riskypabji@gmail.com>
2024-01-20 23:59:37 +0800

    feat: Book Card Section Component

---
+++ src/app/(main)/buku/components/bookDataSection.tsx
@@ -0,0 +1,18 @@
+import { getBook } from "@/services/book"
+import BookCard from "./bookCard"
+import { bukuType } from "@/type/buku"
+
+const BookDataSection = async () => {
+    const data = await getBook()
+    console.log(data)
+    return (
+        <div className="flex flex-col gap-2">
+        
+            {data.map((buku: bukuType, key: number) => (
+                <BookCard {...buku} key={key}/>
+            ))}
+        </div>
+    )
+}
+
+export default BookDataSection
