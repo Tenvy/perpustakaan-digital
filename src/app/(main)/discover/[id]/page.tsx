f36ef9937150767d4c2e421bb4689caa1186efc5
tenvy <riskypabji@gmail.com>
2024-01-20 23:59:09 +0800

    chore: a little bit style for a test

--- src/app/(main)/discover/[id]/page.tsx
+++ src/app/(main)/discover/[id]/page.tsx
@@ -2,10 +2,11 @@ import { getBookById } from "@/services/book"
 
 const Page = async ({ params }: { params: { id: string } }) => {
     const data = await getBookById(params.id)
+    
     return (
-        <>
+        <div className="bg-secondary-color rounded-xl text-primary-color">
             {data.Judul}
-        </>
+        </div>
     )
 }
 
