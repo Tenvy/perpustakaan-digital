93d893f4fe5b732692d1a630b160180c82ad4dfb
tenvy <riskypabji@gmail.com>
2024-01-20 23:58:07 +0800

    feat: DELETE and PATCH http request for buku route

--- src/app/api/buku/[id]/route.ts
+++ src/app/api/buku/[id]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/utils/db";
 import { NextResponse } from "next/server";
+import { bukuType } from "@/type/buku";
 
 export async function GET(request: Request, context: { params: { id: string } } ) {
     const id = context.params.id
@@ -19,6 +20,71 @@ export async function GET(request: Request, context: { params: { id: string } } 
         return NextResponse.json(response)
     } catch (error) {
         console.error('Error fetching data:', error);
+        return NextResponse.json({msg: 'Something went wrong.', error})
+    }
+}
+
+export async function DELETE(request: Request, context: { params: { id: string } } ) {
+    const id = context.params.id
+    try {
+        const parsedId = parseInt(id, 10);
+
+        if (isNaN(parsedId)) {
+            return NextResponse.json({ msg: 'Invalid ID provided.' });
+        }
+
+        const check = await prisma.buku.findFirst({
+            where: {
+                BukuID: parsedId
+            }
+        })
+
+        if (!check) return NextResponse.json({msg: 'Buku Tidak Ada!'})
+        const response = await prisma.buku.delete({
+            where: {
+                BukuID: parsedId
+            }
+        })
+        return NextResponse.json({msg: 'Buku Berhasil Terhapus!', response})
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return NextResponse.json({msg: 'Something went wrong.', error})
+    }
+}
+
+export async function PATCH(request: Request, context: { params: { id: string } } ){
+    const { Judul, Penulis, Penerbit, TahunTerbit, Deskripsi, Gambar }:bukuType = await request.json()
+    const id = context.params.id
+    
+    try {
+        const parsedId = parseInt(id, 10);
+
+        if (isNaN(parsedId)) {
+            return NextResponse.json({ msg: 'Invalid ID provided.' });
+        }
+
+        const check = await prisma.buku.findFirst({
+            where: {
+                BukuID: parsedId
+            }
+        })
+
+        if (!check) return NextResponse.json('Buku Tidak ada!')
+        const response = await prisma.buku.update({
+            where: {
+                BukuID: parsedId
+            },
+            data: {
+                Judul,
+                Penulis,
+                Penerbit,
+                TahunTerbit,
+                Deskripsi,
+                Gambar,
+            }
+        })
+        return NextResponse.json({msg: 'Buku Berhasil Diperbarui!', response})
+    } catch (error) {
         return NextResponse.json({msg: 'Something went wrong.', error})
     }
 }
