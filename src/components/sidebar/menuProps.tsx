e8d1c896b4f88b10f3b5b399e3788d3f4039b036
tenvy <riskypabji@gmail.com>
2024-01-20 23:55:37 +0800

    chore: add Pendataan buku menu

--- src/components/sidebar/menuProps.tsx
+++ src/components/sidebar/menuProps.tsx
@@ -1,6 +1,6 @@
 import { MenuItemProps } from "@/type/sidebarMenu"
 
-import { BiBookContent as BookIcon } from 'react-icons/bi';
+import { BiBookContent as BookIcon, BiPencil as PencilIcon } from 'react-icons/bi';
 
 const iconSize:number = 20;
 
@@ -9,6 +9,12 @@ export const MENU_ITEMS: MenuItemProps[] = [
     title: 'Discover',
     href: '/discover',
     icon: <BookIcon size={iconSize} />,
+    isShow: true,
+  },
+  {
+    title: 'Pendataan Buku',
+    href: '/buku',
+    icon: <PencilIcon size={iconSize} />,
     isShow: true,
   },
 ]; 
