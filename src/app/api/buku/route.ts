dbc6206b7375209baf90a96fd598254ee8177661
tenvy <riskypabji@gmail.com>
2024-01-22 00:05:50 +0800

    feat: add kategori data to post http request on buku route

--- src/app/api/buku/route.ts
+++ src/app/api/buku/route.ts
@@ -7,12 +7,13 @@ export async function GET() {
         const response = await prisma.buku.findMany()
         return NextResponse.json(response)
     } catch (error) {
+        console.error('Error fetching data:', error);
         return NextResponse.json({msg: 'Something went wrong.', error})
     }
 }
 
 export async function POST(request: Request){
-    const { Judul, Penulis, Penerbit, TahunTerbit, Deskripsi, Gambar }:bukuType = await request.json()
+    const { Judul, Penulis, Penerbit, TahunTerbit, Deskripsi, Gambar, Kategori }:bukuType = await request.json()
     try {
         const check = await prisma.buku.findUnique({
             where: {
@@ -20,6 +21,12 @@ export async function POST(request: Request){
             }
         })
         if (check) return NextResponse.json('Buku sudah ada!')
+
+        const categorySet = new Set(Kategori?.map((Kategori) => Kategori.KategoriID));
+        if (Kategori && Kategori.length !== categorySet.size) {
+            return NextResponse.json({ msg: "Duplicate values in Category" });
+        }
+
         const response = await prisma.buku.create({
             data: {
                 Judul,
@@ -28,10 +35,20 @@ export async function POST(request: Request){
                 TahunTerbit,
                 Deskripsi,
                 Gambar,
+                kategoribuku_relasi: {
+                    create: Kategori?.map(kategori => ({
+                        kategoribuku: {
+                            connect: {
+                                KategoriID: kategori.KategoriID
+                            }
+                        }
+                    }))
+                }
             }
         })
         return NextResponse.json({msg: 'Buku Berhasil Dibuat!',response})
     } catch (error) {
+        console.error('Error fetching data:', error);
         return NextResponse.json({msg: 'Something went wrong.', error})
     }
 }
