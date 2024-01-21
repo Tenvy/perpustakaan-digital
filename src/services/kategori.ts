e35727c0c1fe27c3d018060af43c2e3edc74212c
tenvy <riskypabji@gmail.com>
2024-01-22 00:01:29 +0800

    feat: CRUD Kategori Service

---
+++ src/services/kategori.ts
@@ -0,0 +1,60 @@
+
+interface kategori {
+    NamaKategori: string;
+}
+
+const createCategory = async (data: any) => {
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/kategori`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    return errorData.msg;
+  }
+  return response.json();
+};
+
+const getCategory = async () => {
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/kategori`, {
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    return errorData.msg;
+  }
+  return response.json();
+};
+
+const deleteCategory = async (id: string) => {
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/kategori/${id}`, {
+    method: "DELETE",
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    return errorData.msg;
+  }
+  return response.json();
+};
+
+const patchCategory = async (data: kategori, id: string) => {
+  const response = await fetch(process.env.NEXTAUTH_URL + `/api/kategori/${id}`, {
+    method: "PATCH",
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    return errorData.msg;
+  }
+  return response.json();
+};
+
+export { deleteCategory, patchCategory, createCategory, getCategory }
