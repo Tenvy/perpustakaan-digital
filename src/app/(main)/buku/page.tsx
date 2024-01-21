ed6c99f429f9838e86e25d20cc25ed4e2ad1b124
tenvy <riskypabji@gmail.com>
2024-01-22 00:08:25 +0800

    feat: added Create Section Component to Buku Page

--- src/app/(main)/buku/page.tsx
+++ src/app/(main)/buku/page.tsx
@@ -1,8 +1,10 @@
 import BookDataSection from "./components/bookDataSection"
+import CreateSection from "./components/CreateSection"
 
 const Page = () => {
     return (
         <div className="bg-secondary-color rounded-xl p-4 text-primary-color">
+            <CreateSection/>
             <BookDataSection/>
         </div>
     )
