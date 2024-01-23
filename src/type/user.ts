0c69b8e0aa8099794caa471a50c07a4c33ae4598
tenvy <riskypabji@gmail.com>
2024-01-24 00:37:34 +0800

    chore: add peminjaman to user Type

--- src/type/user.ts
+++ src/type/user.ts
@@ -1,7 +1,10 @@
+import { peminjaman } from "@prisma/client";
+
 export type userType = {
     username: string;
     password: string;
     email: string;
     namaLengkap: string;
     alamat: string;
+    peminjaman: peminjaman[];
 }
