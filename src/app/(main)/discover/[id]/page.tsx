258b185f24049e9c4a659f2250d5e35282361345
tenvy <riskypabji@gmail.com>
2024-01-24 00:46:51 +0800

    feat: Discover Page

--- src/app/(main)/discover/[id]/page.tsx
+++ src/app/(main)/discover/[id]/page.tsx
@@ -1,11 +1,18 @@
 import { getBookById } from "@/services/book"
+import DetailSection from "./components/detailSection"
+import PinjamSection from "./components/pinjamSection"
 
 const Page = async ({ params }: { params: { id: string } }) => {
     const data = await getBookById(params.id)
-    
+
     return (
-        <div className="bg-secondary-color rounded-xl text-primary-color">
-            {data.Judul}
+        <div className="flex gap-4">
+            <div className="bg-secondary-color rounded-xl text-primary-color w-full">
+                <DetailSection {...data}/>
+            </div>
+            <div className="w-[30%]">
+                <PinjamSection {...data}/>
+            </div>
         </div>
     )
 }
