c3fff26b8707b1ac9f8087fccc12e269e33f38f3
tenvy <riskypabji@gmail.com>
2024-01-24 00:39:19 +0800

    feat: Sidebar Menu Role Based

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,13 +1,17 @@
 import Breakline from "../elements/breakline"
 import Logo from "./logo";
 import Menu from "./menu"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import { MENU_ITEMS } from './menuProps'
 
 const Sidebar = async () => {
-    const filteredMenu = MENU_ITEMS?.filter((item) => item?.isShow);
-
-
+    const session:any = await getServerSession(authOptions)
+    const filteredMenu = MENU_ITEMS?.filter((item) => {
+        const userAllowed = item.user ? item.user.includes(session?.user?.Role) : true;
+        return item?.isShow && userAllowed;
+      });
     return (
         <header className="w-[35vh] bg-secondary-color">
                 <div className="sticky h-[100vh] transition-all duration-300 top-0 z-10 flex flex-col lg:py-8">
