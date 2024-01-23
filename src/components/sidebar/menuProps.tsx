7efccb27d886f8449f73a083fa4327c5f0954441
tenvy <riskypabji@gmail.com>
2024-01-24 00:39:48 +0800

    feat: add Peminjaman, Laporan, and few user props

--- src/components/sidebar/menuProps.tsx
+++ src/components/sidebar/menuProps.tsx
@@ -10,17 +10,33 @@ export const MENU_ITEMS: MenuItemProps[] = [
     href: '/discover',
     icon: <BookIcon size={iconSize} />,
     isShow: true,
+    
   },
   {
+    title: 'Peminjaman',
+    href: '/pinjaman',
+    icon: <PencilIcon size={iconSize} />,
+    isShow: true,
+  },
+  { 
+    title: 'Laporan',
+    href: '/laporan',
+    icon: <PencilIcon size={iconSize} />,
+    isShow: true,
+    user: ['admin', 'petugas']
+  },
+  { 
     title: 'Pendataan Buku',
     href: '/buku',
     icon: <PencilIcon size={iconSize} />,
     isShow: true,
+    user: ['admin', 'petugas']
   },
   {
     title: 'Pendataan Kategori',
     href: '/kategori',
     icon: <PencilIcon size={iconSize} />,
     isShow: true,
+    user: ['admin', 'petugas']
   },
 ]; 
