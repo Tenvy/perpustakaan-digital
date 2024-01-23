7424e545937c12acce85b261673d8c4972969b8f
tenvy <riskypabji@gmail.com>
2024-01-24 00:38:05 +0800

    feat: Peminjaman type

---
+++ src/type/peminjaman.ts
@@ -0,0 +1,8 @@
+export type peminjamanType = {
+    PeminjamanID: number;
+    UserID: number;
+    BukuID: number;
+    TanggalPeminjaman: Date;
+    TanggalPengembalian: Date;
+    StatusPinjaman: string;
+}
